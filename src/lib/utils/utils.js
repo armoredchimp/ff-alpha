cddf824a3ab5f5572e0bf7402fe39c24f22258fd
Matt K <mattkasmir@gmail.com>
2025-05-04 21:03:05 -0700

    gemini autopick improvement

--- src/lib/utils/utils.js
+++ src/lib/utils/utils.js
@@ -263,10 +263,10 @@ export function populateLineup(team) {
     // Fallback mapping for secondary position assignments
     const fallbackOrder = {
         "Centre Forward":       ["Left Wing", "Right Wing"],
-        "Left Wing":            ["Right Wing", "Centre Forward"],
-        "Right Wing":           ["Left Wing", "Centre Forward"],
-        "Left-Mid":             ["Left Wing", "Central Midfield", "Centre Forward"],
-        "Right-Mid":            ["Right Wing", "Central Midfield", "Centre Forward"],
+        "Left Wing":            ["Right Wing", "Centre Forward","Attacking Midfield"],
+        "Right Wing":           ["Left Wing", "Centre Forward","Attacking Midfield"],
+        "Left-Mid":             ["Left Wing", "Central Midfield", "Centre Forward","Attacking Midfield"],
+        "Right-Mid":            ["Right Wing", "Central Midfield", "Centre Forward","Attacking Midfield"],
         "Central Midfield":     ["Attacking Midfield", "Defensive Midfield"],
         "Attacking Midfield":   ["Central Midfield", "Left Wing", "Right Wing", "Centre Forward"],
         "Defensive Midfield":   ["Centre Back", "Central Midfield", "Left Back", "Right Back"],
