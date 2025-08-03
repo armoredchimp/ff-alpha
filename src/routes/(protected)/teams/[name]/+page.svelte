1cec8ab7c1554d2f353dc02d0890406a1e136659
Matt <mattkasmir@gmail.com>
2025-08-03 11:49:50 -0700

    8/3

--- src/routes/(protected)/teams/[name]/+page.svelte
+++ src/routes/(protected)/teams/[name]/+page.svelte
@@ -1,51 +1,43 @@
-<script>
+<script lang="ts">
   import { onMount } from "svelte";
   import TeamHeader from "$lib/TeamHeader.svelte";
   import TeamScores from "$lib/TeamScores.svelte";
   import Formation from "$lib/Formation.svelte";
   import SelectedList from "$lib/SelectedList.svelte";
   import { createFormationStructure, populateLineup } from "$lib/utils";
-	import { calculateTotalScores } from "$lib/utils";
+  import { calculateTotalScores } from "$lib/utils";
   import { afterNavigate } from '$app/navigation';
-  // import { page} from '$app/state'
+  import type { Team } from '$lib/types/types';
+  import type { PageData } from './$types';
 
-
-  let { data } = $props();
-
-
-
-
+  let { data }: { data: PageData & { team: Team } } = $props();
+    
   afterNavigate(() => {
-
-       data.team.selected = createFormationStructure(data.team.formation);
-       populateLineup(data.team);
-       console.log("FINAL selected:", data.team.selected);
-
-
+    data.team.selected = createFormationStructure(data.team.formation);
+    populateLineup(data.team);
+    console.log("FINAL selected:", data.team.selected);
   });
-
-  
 </script>
-
 
 <div class="page-container">
   <div>
-      <TeamHeader team={data.team} computer={true} />
+    <TeamHeader team={data.team} computer={true} />
   </div>
   <div>
-      <TeamScores scores={data.team.scores.total} playerCount={data.team.playerCount} keeperCount={data.team.keepers.length}/>
+    <TeamScores 
+      scores={data.team.scores.total} 
+      playerCount={data.team.playerCount} 
+      keeperCount={data.team.keepers.length}
+    />
   </div>
-
   <div class="middle-section">
-      <Formation team={data.team} />
-      <SelectedList team={data.team}/>
+    <Formation team={data.team} />
+    <SelectedList team={data.team}/>
   </div>
-
 </div>
-
 
 <style>
   .middle-section {
-      display: flex;
+    display: flex;
   }
 </style>
