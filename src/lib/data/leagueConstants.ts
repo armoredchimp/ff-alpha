2169bda053a61d0b3a4b451eac73ef3b082dbd3e
Matt K <mattkasmir@gmail.com>
2026-08-22 07:09:19 -0700

    8/22

--- src/lib/data/leagueConstants.ts
+++ src/lib/data/leagueConstants.ts
@@ -10,11 +10,11 @@ export const TABLE_PREFIXES: Record<number, string> = {
 
 //THIS IS LAST SEASON (24/25) VALUES, STILL IN PLACE SO NOTHING BREAKS
 export const SEASON_ID_LOOKUP: Record<number, number> = {
-    1: 23614,
-    2: 23621,
-    3: 23744,
-    4: 23643,
-    5: 23746
+    1: 28083,
+    2: 27965,
+    3: 28321,
+    4: 28082,
+    5: 27895
 }
 
 export const LEAGUE_ID_LOOKUP: Record<number, number> = {
